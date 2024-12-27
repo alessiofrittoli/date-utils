93deabb22239e4f2fdfee08ee109e1bc9b76cfd4
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-27 13:19:35 +0100

    tests: add format functions missing branches coverage

--- __tests__/format.test.ts
+++ __tests__/format.test.ts
@@ -15,13 +15,68 @@ describe( 'formatDate', () => {
 
 
 	it( 'formats a date with tokenized parameter', () => {
-		const date		= new Date( '2024-04-20T16:20:00.000Z' )
-		const tokens	= "d - j - D - J - l - w - N - S - z - b - W - m - n - M - F - E - t - L - Y - y - a - A - g - G - h - H - i - s - v - e - O - P - Z - c - U"
-		const expected	= "20 - 20 - Sat - S - Saturday - 6 - 6 - th - 111 - in the evening - 16 - 04 - 4 - Apr - April - A - 30 - 1 - 2024 - 24 - pm - PM - 6 - 18 - 06 - 18 - 20 - 00 - 0 - Europe/Rome - +0200 - +02:00 - 7200 - 2024-04-20T16:20:00.000Z - 1713630000"
+		const date		= new Date( '2024-04-20T16:20:35.420Z' )
+		const tokensMap = {
+			d: '20',
+			j: '20',
+			D: 'Sat',
+			J: 'S',
+			l: 'Saturday',
+			w: '6',
+			N: '6',
+			S: 'th',
+			z: '111',
+			b: 'in the evening',
+			W: '16',
+			m: '04',
+			n: '4',
+			M: 'Apr',
+			F: 'April',
+			E: 'A',
+			t: '30',
+			L: '1',
+			Y: '2024',
+			y: '24',
+			a: 'pm',
+			A: 'PM',
+			B: '723',
+			g: '6',
+			G: '18',
+			h: '06',
+			H: '18',
+			i: '20',
+			s: '35',
+			v: '420',
+			u: '420000',
+			e: 'Europe/Rome',
+			C: 'Central European Summer Time',
+			K: 'Central European Time',
+			Q: 'GMT+02:00',
+			q: 'GMT+0200',
+			R: 'GMT+2',
+			V: 'Italy Time',
+			T: 'GMT+2',
+			I: '1',
+			O: '+0200',
+			P: '+02:00',
+			p: '+02:00',
+			Z: '7200',
+			c: '2024-04-20T16:20:35.420Z',
+			r: 'Sat Apr 20 2024 18:20:35 GMT+0200 (Central European Summer Time)',
+			U: '1713630035.42',
+		}
+		const tokens	= Object.keys( tokensMap ).join( ' - ' )
+		const expected	= Object.values( tokensMap ).join( ' - ' )		
 		
 		expect( formatDate( date, tokens, 'en-US', 'Europe/Rome' ) )
 			.toBe( expected )
 		
+	} )
+
+
+	it( 'returns Z instead of +00:00 with `p` token', () => {
+		expect( formatDate( new Date( '2024-04-20T16:20:35.420Z' ), 'p', 'en-US', 'UTC' ) )
+			.toBe( 'Z' )
 	} )
 
 
@@ -34,6 +89,14 @@ describe( 'formatDate', () => {
 			}, 'en-US' )
 		)
 		expect( formatted ).toBe( 'November 24, 2024' )
+	} )
+
+
+	it( 'formats a date with the current Timezone', () => {
+
+		expect( formatDate( '2024-11-24T21:20:00.000Z', 'e', 'en-US' ) )
+			.toBeTruthy() // cannot test it's result since it depends on the current machine Timezone.
+
 	} )
 
 
@@ -62,21 +125,36 @@ describe( 'secondsToUnit', () => {
 
 	it( 'converts seconds to the right units', () => {
 		
-		const result = secondsToUnit( total )
+		expect( secondsToUnit( total ) )
+			.toStrictEqual( expect.objectContaining( {
+				years			: 3,
+				months			: 2,
+				weeks			: 3,
+				days			: 4,
+				hours			: 4,
+				minutes			: 35,
+				seconds			: 23,
+				milliseconds	: 230,
+				microseconds	: 245,
+			} ) )
 
-		const pass = (
-			result.years === 3 &&
-			result.months === 2 &&
-			result.weeks === 3 &&
-			result.days === 4 &&
-			result.hours === 4 &&
-			result.minutes === 35 &&
-			result.seconds === 23 &&
-			result.milliseconds === 230 &&
-			result.microseconds === 245
-		)		
+	} )
 
-		expect( pass ).toBe( true )
+
+	it( 'converts seconds to the right units with negative value', () => {
+		
+		expect( secondsToUnit( total * -1 ) )
+			.toStrictEqual( expect.objectContaining( {
+				years			: -3,
+				months			: -2,
+				weeks			: -3,
+				days			: -4,
+				hours			: -4,
+				minutes			: -35,
+				seconds			: -23,
+				milliseconds	: -230,
+				microseconds	: -245,
+			} ) )
 
 	} )
 
@@ -101,11 +179,26 @@ describe( 'secondsToUnit', () => {
 
 	} )
 
+
+	it( 'skips weeks with negative value', () => {
+		
+		const result = secondsToUnit( total * -1, true )
+
+		expect( result.weeks ).toBe( null )
+		expect( result.days ).toBe( nDays * -1 )
+
+	} )
+
 } )
 
 describe( 'formatLocaleDate', () => {
 
 	const date = new Date( '2024-04-20T14:20:00.000Z' )
+
+	it( 'formats the current Date', () => {
+		expect( typeof formatLocaleDate() ).toBe( 'string' )
+	} )
+
 
 	it( 'formats a date with custom locale', () => {
 		expect( formatLocaleDate( date, 'it-IT' ) ).toBe( '20 aprile 2024' )
